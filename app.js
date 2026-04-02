731bd193fdf24e7fb7f515910e527b85a7e695e2
nadirkamran--ii <nadir.kamran@ii.co.uk>
2026-04-02 13:54:44 +0100

    add reopended event dispatching

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ module.exports = (app) => {
     app.on("pull_request.opened", async (context) => {
         await dispatchEvent("pull_request.opened", context);
     });
+        
+    app.on("pull_request.reopened", async (context) => {
+        await dispatchEvent("pull_request.reopened", context);
+    });
 };
